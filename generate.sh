0682b22dc1e914bb858fbc5577b106fb7660f805
Ziheng Zhang <allankevinrichie@users.noreply.github.com>
2024-06-09 20:53:23 +0800

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,1 +1,1 @@
-curl -X POST -H "content-type:application/json" -d '{"options":  {"packageName": "eve_netease_api"},"swaggerUrl":"https://ali-esi.evepc.163.com/latest/swagger.json"}' https://generator.swagger.io/api/gen/clients/python
+curl -X POST -H "content-type:application/json" -d '{"options":  {"packageName": "eve_netease_api"},"swaggerUrl":"https://ali-esi.evepc.163.com/latest/swagger.json"}' https://generator.swagger.io/api/gen/clients/python | grep -o '"link":"[^"]*' | cut -d '"' -f 4 | xargs wget -O eve_netease_api.zip
